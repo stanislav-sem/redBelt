b395ef77ee8a0e195f02ae10e7c1ce55f53ab3af
stanislav-sem <crazykvark@gmail.com>
2019-04-08 14:12:50 +0300

    08.04 1

--- deque.h
+++ deque.h
@@ -1,4 +1,5 @@
 #include<vector>
+#include<iostream>
 #include<stdexcept>
 
 using namespace std;
@@ -18,26 +19,88 @@ public:
 
 	T& operator[] (size_t index) {
 		if (index < vFront.size()) {
-			return vFront[index];
-		} else if (index < vFront.size() + vBack.size() - 2) {
-			return vBack[index-vFront.size()];
+			return vFront.at(index);
+		} else if (index < vFront.size() + vBack.size()) {
+			return vBack.at(index - vFront.size());
 		} else {
-			throw out_of_range("no index");
+			throw out_of_range("no index in op[]");
 		}
 	}
-
 	const T& operator[] (size_t index) const {
 		if (index < vFront.size()) {
-			return vFront[index];
-		} else if (index < vFront.size() + vBack.size() - 2) {
-			return vBack[index-vFront.size()];
+			return vFront.at(index);
+		} else if (index < vFront.size() + vBack.size()) {
+			return vBack.at(index - vFront.size());
 		} else {
-			throw out_of_range("no index");
+			throw out_of_range("no index in const op[]");
 		}
 	}
 
-	typename vector<T>::iterator At(size_t index) {
 
+	T& At(size_t index) {
+		if (index < vFront.size()) {
+			return vFront.at(index);
+		} else if (index < vFront.size() + vBack.size()) {
+			return vBack.at(index - vFront.size());
+		} else {
+			throw out_of_range("no index in .At()");
+		}
+	}
+	const T& At(size_t index) const {
+		if (index < vFront.size()) {
+			return vFront.at(index);
+		} else if (index < vFront.size() + vBack.size()) {
+			return vBack.at(index - vFront.size());
+		} else {
+			throw out_of_range("no index in const .At()");
+		}
+	}
+
+
+	const T& Front () const {
+		if (!vFront.empty()) {
+			return vFront[0];
+		} else if (!vBack.empty()) {
+			return vBack[0];
+		} else {
+			throw out_of_range("no index in const Front");
+		}
+	}
+	T& Front() {
+		if (!vFront.empty()) {
+			return vFront[0];
+		} else if (!vBack.empty()) {
+			return vBack[0];
+		} else {
+			throw out_of_range("no index in Front");
+		}
+	}
+
+	const T& Back () const {
+		if (!vBack.empty()) {
+			return vBack[vBack.size()-1];
+		} else if (!vFront.empty()) {
+			return vFront[vFront.size()-1];
+		}
+		else {
+			throw out_of_range("no index in const Back");
+		}
+	}
+	T& Back() {
+		if (!vBack.empty()) {
+			return vBack[vBack.size() - 1];
+		} else if (!vFront.empty()) {
+			return vFront[vFront.size() - 1];
+		} else {
+			throw out_of_range("no index in Back");
+		}
+	}
+
+	void PushFront(const T& value) {
+		vFront.insert(vFront.begin(), value);
+	}
+	void PushBack(const T& value) {
+		vBack.push_back(value);
 	}
 
 private:
--- main.cpp
+++ main.cpp
@@ -10,7 +10,62 @@ void TestEmpty() {
 	ASSERT_EQUAL(d.Size(), 0u);
 }
 
+void TestPush() {
+	Deque<int> d;
+	d.PushBack(42);
+	ASSERT_EQUAL(d.Back(), 42);
+	d.PushBack(66);
+	ASSERT_EQUAL(d.Back(), 66);
+	d.PushFront(22);
+	ASSERT_EQUAL(d.Front(), 22);
+	d.PushFront(33);
+	ASSERT_EQUAL(d.Front(), 33);
+}
+
+void TestBrac() {
+	Deque<int> d;
+	d.PushBack(42);
+	ASSERT_EQUAL(d[0], 42);
+	d.PushBack(66);
+	ASSERT_EQUAL(d[1], 66);
+
+	d.PushFront(22);
+	ASSERT_EQUAL(d[0], 22);
+	d.PushFront(33);
+	ASSERT_EQUAL(d[1], 22);
+	ASSERT_EQUAL(d[0], 33);
+
+	ASSERT_EQUAL(d[2], 42);
+	ASSERT_EQUAL(d[3], 66);
+
+	d[1] = 666;
+	ASSERT_EQUAL(d[1], 666);
+}
+
+void TestAt() {
+	Deque<string> d;
+	d.PushBack("aaa");
+	ASSERT_EQUAL(d.At(0), "aaa");
+	d.PushBack("bbb");
+	ASSERT_EQUAL(d.At(1), "bbb");
+
+	d.PushFront("ccc");
+	ASSERT_EQUAL(d.At(0), "ccc");
+	d.PushFront("ddd");
+	ASSERT_EQUAL(d.At(1), "ccc");
+	ASSERT_EQUAL(d.At(0), "ddd");
+
+	ASSERT_EQUAL(d.At(2), "aaa");
+	ASSERT_EQUAL(d.At(3), "bbb");
+
+	d.At(1) = "zzz";
+	ASSERT_EQUAL(d.At(1), "zzz");
+}
+
 int main() {
 	TestRunner tr;
 	RUN_TEST(tr, TestEmpty);
+	RUN_TEST(tr, TestPush);
+	RUN_TEST(tr, TestBrac);
+	RUN_TEST(tr, TestAt);
 }

9029d39c45eddd82d37a695114a42cedc5032262
stanislav-sem <crazykvark@gmail.com>
2019-04-21 23:13:53 +0300

    Update slow.cpp

--- slow.cpp
+++ slow.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <set>
 
 using namespace std;
 
@@ -10,24 +11,37 @@ public:
   ReadingManager()
       : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
         sorted_users_(),
-        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+        user_positions_(MAX_USER_COUNT_ + 1, -1),
+		page_rating(1000),
+		user_count(0) {}
+
 
   void Read(int user_id, int page_count) {
     if (user_page_counts_[user_id] == 0) {
-      AddUser(user_id);
+//      AddUser(user_id);
+    	page_rating[0].insert(user_id);
+    	++user_count;
     }
     user_page_counts_[user_id] = page_count;
-    int& position = user_positions_[user_id];
-    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-      SwapUsers(position, position - 1);
+    page_rating[page_count].insert(user_id);
+    for (int i = 0; i < page_count; i++) {
+    	auto it = page_rating[i].find(user_id);
+    	if (it != page_rating[i].end()) {
+    		page_rating[i].erase(it);
+    		break;
+    	}
     }
+//    int& position = user_positions_[user_id];
+//    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
+//      SwapUsers(position, position - 1);
+//    }
   }
+
 
   double Cheer(int user_id) const {
     if (user_page_counts_[user_id] == 0) {
       return 0;
     }
-    const int user_count = GetUserCount();
     if (user_count == 1) {
       return 1;
     }
@@ -40,6 +54,15 @@ public:
     if (position == user_count) {
         return 0;
     }
+//    const int page_count = user_page_counts_[user_id];
+//    int position = user_positions_[user_id];
+//    while (position < user_count &&
+//      user_page_counts_[sorted_users_[position]] == page_count) {
+//      ++position;
+//    }
+//    if (position == user_count) {
+//        return 0;
+//    }
     // По умолчанию деление целочисленное, поэтому
     // нужно привести числитель к типу double.
     // Простой способ сделать это — умножить его на 1.0.
@@ -55,6 +78,9 @@ private:
   static const int MAX_USER_COUNT_ = 100'000;
 
   vector<int> user_page_counts_;
+  vector<set<int>> page_rating;
+  int user_count;
+
   vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
   vector<int> user_positions_; // позиции в векторе sorted_users_
 

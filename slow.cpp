993313bb4eb28da6c35d9e67fe58cd29ee88310d
stanislav-sem <crazykvark@gmail.com>
2019-04-22 21:18:46 +0300

    Update slow.cpp

--- slow.cpp
+++ slow.cpp
@@ -6,47 +6,62 @@
 
 using namespace std;
 
+void PrintVecSet(vector<set<int>> input) {
+	cout << '{';
+	for (auto el : input) {
+		cout << '{';
+		for (auto i : el) {
+			cout << i << ',';
+		}
+		cout << '}';
+	}
+	cout << '}' << endl;
+}
+
 class ReadingManager {
 public:
   ReadingManager()
       : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-        page_rating(1000),
-		user_count(0) {}
+        page_rating(1000) {}
 
 
   void Read(int user_id, int page_count) {
-    if (user_page_counts_[user_id] == 0) {
-    	page_rating[0].insert(user_id);
-		++user_count;
-    }
-    user_page_counts_[user_id] = page_count;
-    page_rating[page_count].insert(user_id);
-    for (int i = 0; i < page_count; i++) {
-    	auto it = page_rating[i].find(user_id);
-    	if (it != page_rating[i].end()) {
-    		page_rating[i].erase(it);
-    		break;
-    	}
+//	if (user_page_counts_[user_id] == 0) {
+//		page_rating[0].insert(user_id);
+//	}
+    int tmp_page = user_page_counts_[user_id];  // временно запомнили предыдущую страницу
+    user_page_counts_[user_id] = page_count;    // записали текущую страницу в вектор пользователей
+    page_rating[page_count].insert(user_id);    // записали текущую страницу в вектор страниц
+    auto it = page_rating[tmp_page].find(user_id);       // удалили предыдущю страницу
+    if (it != page_rating[tmp_page].end()) {
+    	page_rating[tmp_page].erase(it);
     }
   }
-
 
   double Cheer(int user_id) const {
     if (user_page_counts_[user_id] == 0) {
       return 0;
     }
-    if (user_count == 1) {
+    int all_reading_users = 0;
+    int less_reading_users = 0;
+    for (int i = 0; i <= 1000; i++ ) {
+    	all_reading_users += page_rating[i].size();
+    	if (i < user_page_counts_[user_id]) {
+    		less_reading_users += page_rating[i].size();
+    	}
+    }
+    if (all_reading_users == 1) {
       return 1;
     }
-    const int page_count = user_page_counts_[user_id];
-    int remain_user = 0;
-    for  (int i = 1; i < page_count; i++) {
-    	remain_user += page_rating[i].size();
-    }
-    // По умолчанию деление целочисленное, поэтому
-    // нужно привести числитель к типу double.
-    // Простой способ сделать это — умножить его на 1.0.
-    return (user_count - remain_user) * 1.0 / (user_count - 1);
+//    const int page_count = user_page_counts_[user_id];
+//    int remain_user = 0;
+//    for  (int i = 1; i < page_count; i++) {
+//    	remain_user += page_rating[i].size();
+//    }
+    PrintVecSet(page_rating);
+//    cout << "all_reading_users: " << all_reading_users << "; less_reading_users: " << less_reading_users << endl;
+//    return (all_reading_users - less_reading_users) * 1.0 / (less_reading_users - 1);
+    return less_reading_users * 1.0 / all_reading_users;
   }
 
 private:
@@ -59,7 +74,7 @@ private:
 
   vector<int> user_page_counts_;
   vector<set<int>> page_rating;
-  int user_count;
+//  int user_count;
 };
 
 
@@ -67,8 +82,8 @@ int main() {
   // Для ускорения чтения данных отключается синхронизация
   // cin и cout с stdio,
   // а также выполняется отвязка cin от cout
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+//  ios::sync_with_stdio(false);
+//  cin.tie(nullptr);
 
   ReadingManager manager;
 
@@ -86,7 +101,8 @@ int main() {
       cin >> page_count;
       manager.Read(user_id, page_count);
     } else if (query_type == "CHEER") {
-      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+//      cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+      cout << setprecision(6) << manager.Cheer(user_id) << endl;
     }
   }
 

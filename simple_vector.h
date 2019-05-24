4c8955c61f55b05903304663915154cf41a300d0
stanislav-sem <44961605+stanislav-sem@users.noreply.github.com>
2019-05-24 16:22:04 +0300

    simple vector 24.05

--- simple_vector.h
+++ simple_vector.h
@@ -6,19 +6,32 @@
 template <typename T>
 class SimpleVector {
 public:
-  SimpleVector();
-  explicit SimpleVector(size_t size);
-  ~SimpleVector();
+  SimpleVector() {
+	  data_ = nullptr;
+	  vecSize = 0;
+  }
+
+  explicit SimpleVector(size_t size) {
+	  data_ = new T[size];
+	  vecSize = size;
+  }
+  ~SimpleVector() {
+	  delete[] data_;
+  }
 
   T& operator[](size_t index);
 
   T* begin();
   T* end();
 
-  size_t Size() const;
+  size_t Size() const {
+	  return vecSize;
+  }
   size_t Capacity() const;
   void PushBack(const T& value);
 
 private:
   // Добавьте поля для хранения данных вектора
+  T* data_;
+  size_t vecSize;
 };

fdc16131694a1e0be53da09fa4c0f7f590e89387
stanislav-sem <crazykvark@gmail.com>
2019-05-27 14:22:47 +0300

    Revert "simple_vector ready"
    This reverts commit b2326763e1d37fb460f07e293ca0439cedd9054d.

--- simple_vector.h
+++ simple_vector.h
@@ -1,65 +1,24 @@
 #pragma once
 
 #include <cstdlib>
-#include <algorithm>
 
 // Реализуйте шаблон SimpleVector
 template <typename T>
 class SimpleVector {
 public:
-  SimpleVector() {
-	  data_ = nullptr;
-	  end_ = nullptr;
-	  endOfArea = nullptr;
-  }
+  SimpleVector();
+  explicit SimpleVector(size_t size);
+  ~SimpleVector();
 
-  explicit SimpleVector(size_t size) {
-	  data_ = new T[size];
-	  end_ = data_ + size;
-	  endOfArea = end_;
-  }
-  ~SimpleVector() {
-	  delete[] data_;
-  }
+  T& operator[](size_t index);
 
-  T& operator[](size_t index) {
-	  return data_[index];
-  }
+  T* begin();
+  T* end();
 
-  T* begin() {return data_;}
-  T* end()   {return end_;}
-  const T* begin() const {return data_;}
-  const T* end()   const {return end_;}
-
-  size_t Size() const {
-	  return end_ - data_;
-  }
-  size_t Capacity() const {
-	  return endOfArea - data_;
-  }
-  void PushBack(const T& value) {
-	  if (Size() == Capacity()) {
-		  T* tmpData = nullptr;
-		  if (Capacity() == 0) {
-			  tmpData = new T[1];
-			  endOfArea = tmpData + 1;
-		  } else {
-				tmpData = new T[2 * Capacity()];
-				endOfArea = tmpData + 2 * Capacity();
-		  }
-		  T* tmp_end_ = std::copy(data_, end_, tmpData);
-		  delete[] data_;
-		  data_ = tmpData;
-		  *tmp_end_ = value;
-		  end_ = ++tmp_end_;
-	  } else {
-		  *end_ = value;
-		  end_++;
-	  }
-  }
+  size_t Size() const;
+  size_t Capacity() const;
+  void PushBack(const T& value);
 
 private:
-  T* data_;
-  T* end_;
-  T* endOfArea;
+  // Добавьте поля для хранения данных вектора
 };

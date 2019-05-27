20b8f19b3dc49f40dde0758ec9962c321cf30642
stanislav-sem <crazykvark@gmail.com>
2019-05-27 14:24:08 +0300

    simple_vector ready

--- simple_vector.h
+++ simple_vector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdlib>
+#include <algorithm>
 
 // Реализуйте шаблон SimpleVector
 template <typename T>
@@ -8,30 +9,57 @@ class SimpleVector {
 public:
   SimpleVector() {
 	  data_ = nullptr;
-	  vecSize = 0;
+	  end_ = nullptr;
+	  endOfArea = nullptr;
   }
 
   explicit SimpleVector(size_t size) {
 	  data_ = new T[size];
-	  vecSize = size;
+	  end_ = data_ + size;
+	  endOfArea = end_;
   }
   ~SimpleVector() {
 	  delete[] data_;
   }
 
-  T& operator[](size_t index);
+  T& operator[](size_t index) {
+	  return data_[index];
+  }
 
-  T* begin();
-  T* end();
+  T* begin() {return data_;}
+  T* end()   {return end_;}
+  const T* begin() const {return data_;}
+  const T* end()   const {return end_;}
 
   size_t Size() const {
-	  return vecSize;
+	  return end_ - data_;
   }
-  size_t Capacity() const;
-  void PushBack(const T& value);
+  size_t Capacity() const {
+	  return endOfArea - data_;
+  }
+  void PushBack(const T& value) {
+	  if (Size() == Capacity()) {
+		  T* tmpData = nullptr;
+		  if (Capacity() == 0) {
+			  tmpData = new T[1];
+			  endOfArea = tmpData + 1;
+		  } else {
+				tmpData = new T[2 * Capacity()];
+				endOfArea = tmpData + 2 * Capacity();
+		  }
+		  T* tmp_end_ = std::copy(data_, end_, tmpData);
+		  delete[] data_;
+		  data_ = tmpData;
+		  *tmp_end_ = value;
+		  end_ = ++tmp_end_;
+	  } else {
+		  *end_ = value;
+		  end_++;
+	  }
+  }
 
 private:
-  // Добавьте поля для хранения данных вектора
   T* data_;
-  size_t vecSize;
+  T* end_;
+  T* endOfArea;
 };

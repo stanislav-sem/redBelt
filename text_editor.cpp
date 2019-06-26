6460519aa04703b50bc580f2e250f59d1fc5378b
stanislav-sem <crazykvark@gmail.com>
2019-06-26 16:01:44 +0300

    Update text_editor.cpp

--- text_editor.cpp
+++ text_editor.cpp
@@ -1,96 +1,62 @@
 #include <string>
-#include <string_view>
-#include <list>
+#include<list>
 #include "test_runner.h"
 using namespace std;
 
-using IT = list<char>::iterator;
-
 class Editor {
-public:
-<<<<<<< HEAD
-  Editor() : text{} {
-	  pos = 0;
+ public:
+  // Реализуйте конструктор по умолчанию и объявленные методы
+  Editor(): textdata{}, buffer{} {
+	  curPos = textdata.begin();
   }
 
-  // сдвинуть курсор влево
   void Left() {
-	  if (pos != 0) {
-		  pos--;
+	  if (curPos != textdata.begin())
+	  {
+		  curPos--;
 	  }
   }
 
-  // сдвинуть курсор вправо
-  void Right() {
-	  if (pos != text.size()) {
-		  pos++;
+  void Right(size_t input = 1) {
+	  if (curPos != textdata.end()) {
+		  advance(curPos, input);
 	  }
   }
 
-  // вставить символ token
   void Insert(char token) {
-	  text.insert(GetIt(), token);
-   }
-=======
-  Editor();
-  // сдвинуть курсор влево
-  void Left(){
-	  text = {};
-	  pos = 0;
-  };
+	  textdata.insert(curPos, token);
+  }
 
-  // сдвинуть курсор вправо
-  void Right();
+  void Cut(size_t tokens = 1) {
+	  list<char>::iterator tmpPos{curPos};
+	  advance(tmpPos, tokens);
+//	  buffer.splice(buffer.begin(), textdata, curPos, tmpPos);
+	  buffer = {curPos,  tmpPos};
+	  textdata.erase(curPos, tmpPos);
+  }
 
-  // вставить символ token
-  void Insert(char token);
->>>>>>> parent of 5ecb0c8... Update text_editor.cpp
+  void Copy(size_t tokens = 1) {
+	  list<char>::iterator tmpPos{curPos};
+	  advance(tmpPos, tokens);
+	  buffer = {curPos, tmpPos};
+  }
 
-  // cкопировать не более tokens символов, начиная с текущей позиции курсора
-  void Copy(size_t tokens);
+  void Paste() {
+	  textdata.insert(curPos, buffer.begin(), buffer.end());
+  }
 
-  // вырезать не более tokens символов, начиная с текущей позиции курсора
-<<<<<<< HEAD
-  void Cut(size_t tokens) {
-	  for (size_t i = 0; i < tokens; ++i) {
-		  buffer.push_back(*pos);
-		  text.erase(pos);
-	  }
-//	  cout << "Text: " << GetText() << endl;
-   }
-=======
-  void Cut(size_t tokens);
->>>>>>> parent of 5ecb0c8... Update text_editor.cpp
-
-  // вставить содержимое буфера в текущую позицию курсора
-  void Paste();
-
-  // получить текущее содержимое текстового редактора
-<<<<<<< HEAD
   string GetText() const {
-	  return {text.begin(), text.end()};
+	  return {textdata.begin(), textdata.end()};
   }
 
-private:
-  list<char> text;
+  string GetBuffer() const {
+	  return {buffer.begin(), buffer.end()};
+  }
+
+ private:
+  list<char> textdata;
   list<char> buffer;
-  size_t pos;
-
-  IT GetIt(size_t input = pos) {
-	  IT result = text.begin();
-	  for (size_t i = 0; i != input; i++) {
-		  result++;
-	  }
-	  return result;
-  }
-=======
-  string GetText() const;
-
-private:
-  list<char> text;
-  size_t pos;
->>>>>>> parent of 5ecb0c8... Update text_editor.cpp
-
+  list<char>::iterator curPos;
 };
 
 void TypeText(Editor& editor, const string& text) {
@@ -101,30 +67,32 @@ void TypeText(Editor& editor, const string& text) {
 
 void TestEditing() {
   {
-	Editor editor;
+    Editor editor;
 
     const size_t text_len = 12;
     const size_t first_part_len = 7;
     TypeText(editor, "hello,.world");
-//    cout << editor.GetText() << endl;
-//    cout << "Current pos: " << editor.GetPos() << endl;
     for(size_t i = 0; i < text_len; ++i) {
       editor.Left();
     }
-
-
     editor.Cut(first_part_len);
-    cout << editor.GetText() << endl;
-//    cout << "Current pos: " << editor.GetPos() << endl;
-
+    cout << "text: " << editor.GetText() << endl;
+    cout << "buffer: " << editor.GetBuffer() << endl;
     for(size_t i = 0; i < text_len - first_part_len; ++i) {
       editor.Right();
     }
     TypeText(editor, ",.");
+    cout << "Typing" << endl;
+    cout << "text: " << editor.GetText() << endl;
+    cout << "buffer: " << editor.GetBuffer() << endl;
     editor.Paste();
+    cout << "Pasting" << endl;
+    cout << "text: " << editor.GetText() << endl;
+    cout << "buffer: " << editor.GetBuffer() << endl;
     editor.Left();
     editor.Left();
     editor.Cut(3);
+    
     ASSERT_EQUAL(editor.GetText(), "world,.hello");
   }
   {
@@ -189,11 +157,10 @@ void TestEmptyBuffer() {
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestEditing);
-  RUN_TEST(tr, TestReverse);
-  RUN_TEST(tr, TestNoText);
-  RUN_TEST(tr, TestEmptyBuffer);
-
-  return 0;
+	TestRunner tr;
+	RUN_TEST(tr, TestEditing);
+	RUN_TEST(tr, TestReverse);
+	RUN_TEST(tr, TestNoText);
+	RUN_TEST(tr, TestEmptyBuffer);
+	return 0;
 }
